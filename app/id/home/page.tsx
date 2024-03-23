304c1a83c115bb143fb7b2dd9131de9fbc409bce
alief abdillah <aliefmabdillah09@gmail.com>
2024-03-23 13:11:55 +0700

    feat: scrolled action navbar and viewport

--- app/id/home/page.tsx
+++ app/id/home/page.tsx
@@ -7,12 +7,17 @@ export default function Homepage() {
   return (
     <div>
       <NavbarWhite/>
-      <div>Hello, Next.JS!</div>
+      {/* <div className='bg-red-500 h-screen'>Hello, Next.JS!</div> */}
+      <div className='flex flex-col items-center w-full h-screen max-h-svh bg-slate-600'>
+        <div className="w-8/12 bg-red-500">
+          Tes
+        </div>
+      </div>
+      {/* <div className='w-full h-screen bg-slate-600'>TES</div> */}
+      {/* <div className='w-full h-44 bg-slate-600'>TES</div>
       <div className='w-full h-44 bg-slate-600'>TES</div>
       <div className='w-full h-44 bg-slate-600'>TES</div>
-      <div className='w-full h-44 bg-slate-600'>TES</div>
-      <div className='w-full h-44 bg-slate-600'>TES</div>
-      <div className='w-full h-44 bg-slate-600'>TES</div>
+      <div className='w-full h-44 bg-slate-600'>TES</div> */}
       <Footer />
     </div>
   )
--- components/navbarWhite.tsx
+++ components/navbarWhite.tsx
@@ -1,12 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Profile from "./navbar/profile";
 
 export default function NavbarWhite() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
+
   return (
-    <div className="navbar bg-transparent">
+    <div className={`
+      sticky top-0
+      navbar 
+      bg-transparent
+      ${isScrolled ? 'bg-white' : ''} ${isScrolled ? 'shadow-lg' : ''} ${isScrolled ? 'h-10' : ''}
+    `}>
       <div className="navbar-start flex items-center justify-between w-full lg:w-auto">
         <div className="items-start dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -67,7 +91,7 @@ export default function NavbarWhite() {
         </Link>
         <button
           className="btn btn-ghost lg:hidden"
-          onClick={() => document.getElementById("search_modal_sm").showModal()}
+          onClick={() => (document.getElementById("search_modal_sm")! as HTMLDialogElement).showModal()}
         >
           <MagnifyingGlassIcon className="mx-4 lg:hidden h-5 w-5 fill-primary" />
         </button>
@@ -135,7 +159,7 @@ export default function NavbarWhite() {
       <div className="navbar-end hidden mx-4 lg:flex w-fit ">
         <button
           className="btn btn-circle bg-transparent border-none hover:bg-primary"
-          onClick={() => document.getElementById("search_modal").showModal()}
+          onClick={() => (document.getElementById("search_modal")! as HTMLDialogElement).showModal()}
         >
           <MagnifyingGlassIcon className="h-5 w-5 fill-primary hover:fill-white" />
         </button>
@@ -156,74 +180,7 @@ export default function NavbarWhite() {
           </form>
         </dialog>
       </div>
-      <div className="flex lg:mr-8">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar hover:!bg-primary"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="hover:!bg-primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Profil
-              </a>
-            </li>
-            <li>
-              <a className="hover:!bg-primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path d="M9.375 3a1.875 1.875 0 0 0 0 3.75h1.875v4.5H3.375A1.875 1.875 0 0 1 1.5 9.375v-.75c0-1.036.84-1.875 1.875-1.875h3.193A3.375 3.375 0 0 1 12 2.753a3.375 3.375 0 0 1 5.432 3.997h3.943c1.035 0 1.875.84 1.875 1.875v.75c0 1.036-.84 1.875-1.875 1.875H12.75v-4.5h1.875a1.875 1.875 0 1 0-1.875-1.875V6.75h-1.5V4.875C11.25 3.839 10.41 3 9.375 3ZM11.25 12.75H3v6.75a2.25 2.25 0 0 0 2.25 2.25h6v-9ZM12.75 12.75v9h6.75a2.25 2.25 0 0 0 2.25-2.25v-6.75h-9Z" />
-                </svg>
-                10 Points
-              </a>
-            </li>
-            <li>
-              <a className="hover:!bg-primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                Keluar
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Profile />
     </div>
   );
 }
