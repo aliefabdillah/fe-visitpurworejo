b389ee3fc1ddd08372993bd9704bf3c95c585a8f
alief abdillah <aliefmabdillah09@gmail.com>
2024-04-02 19:44:16 +0700

    feat: slicing tentang kami page

---
+++ components/hero/hero-image.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import React, { CSSProperties, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
+
+export default function HeroImage({ image }: {image?: string[]}) {
+  let length = 1
+  if (image) {
+    length = image.length
+  }
+
+  return (
+    <div className="z-1">
+      <Swiper
+        effect="fade"
+        spaceBetween={50}
+        pagination={{
+          // dynamicBullets: true,
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Pagination, EffectFade]}
+        className="mySwiper h-[25vh] md:h-[30vh] lg:h-[35vh] xl:h-[75vh] 2xl:h-[85vh]"
+        style={
+          {
+            "--swiper-pagination-color": "#F5AA27",
+            "--swiper-pagination-bullet-inactive-color": "#A0A0A0",
+            "--swiper-pagination-bullet-size": "12px",
+            "--swiper-pagination-bullet-inactive-opacity": "1",
+            "--swiper-pagination-bottom": "24px",
+            "--swiper-pagination-bullet-horizontal-gap": "15px",
+          } as CSSProperties
+        }
+      >
+        {Array.from({ length: length }).map((_, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="hero h-full"
+              style={{
+                backgroundImage:
+                  "url(https://images.unsplash.com/photo-1503965830912-6d7b07921cd1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-50"></div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
--- components/introSection.tsx
+++ components/introSection.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
-export default function IntroSection({ title, body }: { title: string, body?: string}) {
+export default function IntroSection({
+  title,
+  body = `There are many variations of passages of Lorem Ipsum available, but the
+  majority have suffered alteration in some form, by injected humour, or
+  randomised words which dont look even slightly believable. If you are
+  going to use a passage of Lorem Ipsum, you need to be sure there isnt
+  anything embarrassing hidden in the middle of text.`,
+  titleClass = "text-5xl text-primary mb-4",
+}: {
+  title: string;
+  body?: string;
+  titleClass?: string;
+}) {
   return (
     <>
-      <h1 className="text-5xl font-extrabold text-primary mb-4">{title}</h1>
-      <p className="font-normal text-lg">
-        There are many variations of passages of Lorem Ipsum available, but the
-        majority have suffered alteration in some form, by injected humour, or
-        randomised words which dont look even slightly believable. If you are
-        going to use a passage of Lorem Ipsum, you need to be sure there isnt
-        anything embarrassing hidden in the middle of text.
-      </p>
+      <h1 className={`font-extrabold ${titleClass}`}>{title}</h1>
+      {body.split("<br>").map((paragraph, index) => (
+        <p key={index} className="font-normal text-lg">
+          {paragraph}<br/><br/>
+        </p>
+      ))}
     </>
   );
 }
