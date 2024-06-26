5312d880312236c1a4e700a717293bed89025e3c
alief abdillah <aliefmabdillah09@gmail.com>
2024-06-26 09:14:23 +0700

    chore: set html dangerous  component

--- components/id/gallery/modalGallery.tsx
+++ components/id/gallery/modalGallery.tsx
@@ -6,7 +6,13 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
-export default function ModalGallery({ galleryData, onClose }: { galleryData?: Wisata, onClose?: () => void }) {
+export default function ModalGallery({
+  galleryData,
+  onClose,
+}: {
+  galleryData?: Wisata;
+  onClose?: () => void;
+}) {
   const handleBackButtonState = () => {
     if (onClose) {
       onClose();
@@ -26,11 +32,11 @@ export default function ModalGallery({ galleryData, onClose }: { galleryData?: W
 
     fetchDictionary();
   }, [lang, query, searchParams]);
-  
+
   return (
     <dialog id="gallery_modal" className="modal modal-middle">
       <div className="modal-box p-0 max-w-130">
-      <form method="dialog">
+        <form method="dialog">
           <button
             onClick={handleBackButtonState}
             className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
@@ -47,17 +53,22 @@ export default function ModalGallery({ galleryData, onClose }: { galleryData?: W
               src={
                 galleryData?.img_cover?.url
                   ? galleryData.img_cover.url
-                  : "https://placehold.jp/120/EEEEEE/D0D0D0/900x600.png?text=No+Image"
+                  : "https://placehold.jp/120/EEEEEE/D0D0D0/900x600.png?text=..."
               }
               alt="Wisata Image"
               className="basis-6/12 h-64 md:h-100 md:object-cover object-fit w-screen md:w-full"
             />
           </div>
           <div className="flex-col w-full p-8 basis-6/12 text-wrap">
-            <p className="mb-3 text-4xl font-extrabold overflow-hidden">{galleryData?.name}</p>
-            <p className="mb-3 line-clamp-5 lg:line-clamp-4 overflow-hidden">
-              {galleryData?.deskripsi}
+            <p className="mb-3 text-4xl font-extrabold overflow-hidden">
+              {galleryData?.name}
             </p>
+            <div
+              className="font-normal text-xl mb-3 prose line-clamp-5 lg:line-clamp-4 overflow-hidden"
+              dangerouslySetInnerHTML={{
+                __html: galleryData?.deskripsi ? galleryData.deskripsi : "",
+              }}
+            />
             <Link href={`/destinasi/${galleryData?.slug}`}>
               <button className="btn btn-sm btn-secondary text-white w-full">
                 {intl ? intl.gallery.buttonText : ""}
